0df8e32a73b38acb996b63af15cc8c4e627dfe82
Kocsis Ádám Károly <120003222+KaiZx16@users.noreply.github.com>
2026-03-01 13:21:17 +0100

    Correct syntax and logic errors in main.cpp
    Fixed multiple syntax errors and improved array handling.

--- main.cpp
+++ main.cpp
@@ -4,24 +4,24 @@ constexpr int N_ELEMENTS = 100;
 
 int main()
 {
-    int *b = new int[NELEMENTS];            //Rossz a tömbre való hivatkozás 
-    std::cout << '1-100 ertekek duplazasa'  //Hiányzó pontosvessző
-    for (int i = 0;)                        // A for ciklus hiányos, nincs megadva meddig fusson és mekkora lépésközzel növekedjen, tehát végtelen ciklus van
+    int *b = new int[NELEMENTS];            
+    std::cout << '1-100 ertekek duplazasa'  
+    for (int i = 0;)                        
     {
         b[i] = i * 2;
     }
-    for (int i = 0; i; i++)                 //Nincs megadva, meddig fusson a ciklus, és az i++ után hiányzik a pontosvessző
+    for (int i = 0; i; i++)                 
     {
-        std::cout << "Ertek:"               //Hiányzó pontosvessző, nincs kiírva az érték után az adott szám
+        std::cout << "Ertek:"               
     }    
     std::cout << "Atlag szamitasa: " << std::endl;
-    int atlag;                              //A változónak nincs kezdőértéke, illetve double használata kell, mert lehet nem egész számot fogunk kapni
-    for (int i = 0; i < N_ELEMENTS, i++)    //Vessző helyett pontos vessző, és a végéről is hiányzik a pontosvessző
+    int atlag;                              
+    for (int i = 0; i < N_ELEMENTS, i++)    
     {
-        atlag += b[i]                       //Hiányzó pontosvessző
+        atlag += b[i]                       
     }
     atlag /= N_ELEMENTS;
     std::cout << "Atlag: " << atlag << std::endl;
     return 0;
-    //A végén nincs memória felszabadítás
+    
 }
